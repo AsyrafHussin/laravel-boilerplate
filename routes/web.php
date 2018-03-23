a90758cbc3b310efe642a38e2648ae25d6bc8199
Asyraf Hussin <asyrafhussin4@gmail.com>
2018-03-24 00:19:31 +0800

    Update route

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+/**
+ * Auth
+ */
+Route::name('auth:')->group(function () {
+    Route::middleware('guest')->group(function() {
+        Route::name('login')->get('login', 'AuthController@index');
+        Route::name('login:check')->post('login/check', 'AuthController@auth');       
+    });   
+    Route::name('logout')->middleware('auth')->get('logout', 'AuthController@logout');              
+});
+
+// sample route
+// Route::name('test:')->prefix('test')->group(function () {
+//     Route::name('index')->get('/', 'TestController@index');
+//     Route::name('save')->post('save', 'TestController@save');
+//     Route::name('edit')->get('edit/{id}', 'TestController@edit');
+//     Route::name('update')->post('update/{id}', 'TestController@update');
+//     Route::name('delete')->get('delete/{id}', 'TestController@delete');
+// }); 
