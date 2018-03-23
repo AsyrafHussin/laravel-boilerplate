e118325920a7d5b3327ef444eae821b3e218b521
Asyraf Hussin <asyrafhussin4@gmail.com>
2018-03-23 23:45:22 +0800

    Add bouncer package

--- app/User.php
+++ app/User.php
@@ -3,11 +3,13 @@
 namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Silber\Bouncer\Database\HasRolesAndAbilities;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasRolesAndAbilities;
 
     /**
      * The attributes that are mass assignable.
